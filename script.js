461a082211d416211306676c262f0877c4b028f3
Gen Yap Feng Yuan <58755531+TheKinng96@users.noreply.github.com>
2020-06-17 18:35:31 +0900

    refined the weather part and html content

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 // @ts-nocheck
-const filtered = [];
+
 // weather
 let API_KEY = "a8e71c9932b20c4ceb0aed183e6a83bb";
 
 const getWeatherData = (newPostCode) => {
   const URL = "https://api.openweathermap.org/data/2.5/forecast";
   const FULL_URL = `${URL}?zip=${newPostCode},jp&appid=${API_KEY}`
-  console.log(FULL_URL)
+  // console.log(FULL_URL)
   const weatherPromise = fetch(FULL_URL);
   return weatherPromise.then((response) => {
     return response.json();
@@ -14,7 +14,7 @@ const getWeatherData = (newPostCode) => {
 }
 
 const searchZipcode = () => {
-
+  const filtered = [];
   const postCode = document.getElementById("postcode").value;
   const newPostCode = `${postCode.substring(0,3)}-${postCode.substring(3,8)}`
   // console.log(newPostCode)
@@ -23,17 +23,18 @@ const searchZipcode = () => {
     filtered.push(response.list[1])
     filtered.push(response.list[9])
     filtered.push(response.list[17])
-    console.log(filtered)
-    showWeatherForecast(filtered);
+    // console.log(filtered)
+    showWeatherForecast(filtered,response);
   }).catch ((err)=>{
     console.log(err)
   })
   
 }
 
-const showWeatherForecast = (filtered) => {
+const showWeatherForecast = (filtered,response) => {
   let weatherHtml = '';
-  let PNGurl = 'http://openweathermap.org/img/wn/'
+  let allDays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+  let months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
   filtered.map((i, index) => {
     weatherHtml += `
     <div class="forecast">
@@ -42,8 +43,8 @@ const showWeatherForecast = (filtered) => {
         </div>
         <div class="weather-info">
           <div class="date-info">
-            <h4 class="date">${new Date(i['dt']).toLocaleDateString("sg")}</h4>
-            <h4 class="day">Tues</h4>
+            <h4 class="date">${(new Date(i['dt'] * 1000)).getUTCDate()} ${months[(new Date(i['dt'] * 1000)).getMonth()]} ${(new Date(i['dt'] * 1000)).getFullYear()}</h4>
+            <h4 class="day">${allDays[(new Date(i['dt'] * 1000)).getDay()]}</h4>
           </div>
           <div class="weather">${i['weather']['0']['main']}</div>
           <div class="max-min">
@@ -52,8 +53,9 @@ const showWeatherForecast = (filtered) => {
           </div>
         </div>
       </div>
-    `
+    `;
   })
+  document.getElementById('city-name').innerHTML = response.city.name + ', ' + response.city.country
   document.querySelector('.forecast-container').innerHTML = weatherHtml;
 }
 
